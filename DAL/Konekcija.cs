7a662ccce1d9d593b54bf6959ba3f6fac6a8cc03
lucwii <milanoviclukaa23@gmail.com>
2026-03-25 16:31:30 +0100

    feat: dto, connection and dependencies added

--- DAL/Konekcija.cs
+++ DAL/Konekcija.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace DAL
 {
     internal class Konekcija
     {
+        public static SqlConnection GetKonekcija()
+        {
+            string connStr = ConfigurationManager.ConnectionStrings["FilmoviDB"].ConnectionString;
+            return new SqlConnection(connStr);
+        }
     }
 }
---
+++ DTO/ZanrDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    internal class ZanrDTO
+    {
+        public int idZanr { get; set; }
+        public string NazivZanra { get; set; }
+    }
+}
