75cbd989defc1f54fb6d9505c6cc6d1a18278dad
lucwii <milanoviclukaa23@gmail.com>
2026-03-30 16:33:07 +0200

    feat: recenzija dto added

---
+++ DTO/RecenzijaDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class RecenzijaDTO
+    {
+        public int IdRecenzije { get; set; }
+        public string TekstRecenzije { get; set; }
+        public int Ocena { get; set; }
+        public DateTime DatumRecenzije { get; set; }
+        public int IdFilm { get; set; }
+    }
+}
