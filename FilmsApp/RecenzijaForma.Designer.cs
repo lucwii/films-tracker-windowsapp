cbe0268bdd23699a22d8d39fa9f13fca089d7b3c
lucwii <milanoviclukaa23@gmail.com>
2026-03-30 17:31:57 +0200

    feat: recenzija form finished

---
+++ FilmsApp/RecenzijaForma.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace FilmsApp
+{
+    partial class RecenzijaForma
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbNaslov = new System.Windows.Forms.Label();
+            this.dgvRecenzije = new System.Windows.Forms.DataGridView();
+            this.btnNovi = new System.Windows.Forms.Button();
+            this.btnIzmeni = new System.Windows.Forms.Button();
+            this.btnObrisi = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRecenzije)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbNaslov
+            // 
+            this.lbNaslov.AutoSize = true;
+            this.lbNaslov.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNaslov.Location = new System.Drawing.Point(36, 38);
+            this.lbNaslov.Name = "lbNaslov";
+            this.lbNaslov.Size = new System.Drawing.Size(142, 24);
+            this.lbNaslov.TabIndex = 0;
+            this.lbNaslov.Text = "Recenzije filma:";
+            // 
+            // dgvRecenzije
+            // 
+            this.dgvRecenzije.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvRecenzije.Location = new System.Drawing.Point(40, 82);
+            this.dgvRecenzije.MultiSelect = false;
+            this.dgvRecenzije.Name = "dgvRecenzije";
+            this.dgvRecenzije.ReadOnly = true;
+            this.dgvRecenzije.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvRecenzije.Size = new System.Drawing.Size(402, 260);
+            this.dgvRecenzije.TabIndex = 1;
+            // 
+            // btnNovi
+            // 
+            this.btnNovi.Location = new System.Drawing.Point(40, 360);
+            this.btnNovi.Name = "btnNovi";
+            this.btnNovi.Size = new System.Drawing.Size(75, 33);
+            this.btnNovi.TabIndex = 2;
+            this.btnNovi.Text = "Novi";
+            this.btnNovi.UseVisualStyleBackColor = true;
+            this.btnNovi.Click += new System.EventHandler(this.btnNovi_Click);
+            // 
+            // btnIzmeni
+            // 
+            this.btnIzmeni.Location = new System.Drawing.Point(204, 360);
+            this.btnIzmeni.Name = "btnIzmeni";
+            this.btnIzmeni.Size = new System.Drawing.Size(75, 33);
+            this.btnIzmeni.TabIndex = 3;
+            this.btnIzmeni.Text = "Izmeni";
+            this.btnIzmeni.UseVisualStyleBackColor = true;
+            this.btnIzmeni.Click += new System.EventHandler(this.btnIzmeni_Click);
+            // 
+            // btnObrisi
+            // 
+            this.btnObrisi.Location = new System.Drawing.Point(367, 360);
+            this.btnObrisi.Name = "btnObrisi";
+            this.btnObrisi.Size = new System.Drawing.Size(75, 33);
+            this.btnObrisi.TabIndex = 4;
+            this.btnObrisi.Text = "Obrisi";
+            this.btnObrisi.UseVisualStyleBackColor = true;
+            this.btnObrisi.Click += new System.EventHandler(this.btnObrisi_Click);
+            // 
+            // RecenzijaForma
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 417);
+            this.Controls.Add(this.btnObrisi);
+            this.Controls.Add(this.btnIzmeni);
+            this.Controls.Add(this.btnNovi);
+            this.Controls.Add(this.dgvRecenzije);
+            this.Controls.Add(this.lbNaslov);
+            this.Name = "RecenzijaForma";
+            this.Text = "RecenzijaForma";
+            this.Load += new System.EventHandler(this.RecenzijaForma_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRecenzije)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbNaslov;
+        private System.Windows.Forms.DataGridView dgvRecenzije;
+        private System.Windows.Forms.Button btnNovi;
+        private System.Windows.Forms.Button btnIzmeni;
+        private System.Windows.Forms.Button btnObrisi;
+    }
+}
---
+++ FilmsApp/RecenzijaForma.cs
@@ -0,0 +1,91 @@
+﻿using Biznis;
+using DTO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FilmsApp
+{
+    public partial class RecenzijaForma : Form
+    {
+        private RecenzijaBiznis recenzijaBiznis = new RecenzijaBiznis();
+        private FilmDTO trenutniFilm;
+        public RecenzijaForma(FilmDTO film)
+        {
+            InitializeComponent();
+            trenutniFilm = film;
+            lbNaslov.Text = "Recenzije filma: " + trenutniFilm.Naziv;
+        }
+
+        private void RecenzijaForma_Load(object sender, EventArgs e)
+        {
+            UcitajPodatke();
+        }
+
+        private void UcitajPodatke()
+        {
+            try
+            {
+                dgvRecenzije.DataSource = recenzijaBiznis.GetAll(trenutniFilm.IdFilm);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Greška pri učitavanju: " + ex.Message,
+                   "Greška", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnNovi_Click(object sender, EventArgs e)
+        {
+            RecenzijaDetaljiForma forma = new RecenzijaDetaljiForma(trenutniFilm);
+            forma.ShowDialog();
+            UcitajPodatke();
+        }
+
+        private void btnIzmeni_Click(object sender, EventArgs e)
+        {
+            if(dgvRecenzije.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Molimo izaberite jednu recenziju za izmenu!", "Upozorenje", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            RecenzijaDTO selektovana = (RecenzijaDTO)dgvRecenzije.SelectedRows[0].DataBoundItem;
+            RecenzijaDetaljiForma forma = new RecenzijaDetaljiForma(trenutniFilm, selektovana);
+            forma.ShowDialog();
+            UcitajPodatke();
+        }
+
+        private void btnObrisi_Click(object sender, EventArgs e)
+        {
+            if(dgvRecenzije.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Molimo izaberite jednu recenziju za brisanje!", "Upozorenje", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            RecenzijaDTO selektovana = (RecenzijaDTO)dgvRecenzije.SelectedRows[0].DataBoundItem;
+
+            DialogResult potvrda = MessageBox.Show("Da li ste sigurni da zelite da obrisete ovu recenziju?", "Potvrda brisanja", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if(potvrda == DialogResult.Yes)
+            {
+                try
+                {
+                    recenzijaBiznis.Delete(selektovana.IdRecenzije);
+                    MessageBox.Show("Recenzija uspesno obrisana!", "Uspeh", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    UcitajPodatke();
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message,
+                        "Greška", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
