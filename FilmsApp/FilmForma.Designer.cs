6f5de16119e4417588f66718c08cd7cd0b0293e3
lucwii <milanoviclukaa23@gmail.com>
2026-03-30 15:10:28 +0200

    feat: created film form and the logic behind it

---
+++ FilmsApp/FilmForma.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace FilmsApp
+{
+    partial class FilmForma
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbNaslov = new System.Windows.Forms.Label();
+            this.dgvFilmovi = new System.Windows.Forms.DataGridView();
+            this.btnNovi = new System.Windows.Forms.Button();
+            this.btnIzmeni = new System.Windows.Forms.Button();
+            this.btnObrisi = new System.Windows.Forms.Button();
+            this.btnRecenzije = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFilmovi)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbNaslov
+            // 
+            this.lbNaslov.AutoSize = true;
+            this.lbNaslov.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNaslov.Location = new System.Drawing.Point(49, 31);
+            this.lbNaslov.Name = "lbNaslov";
+            this.lbNaslov.Size = new System.Drawing.Size(126, 24);
+            this.lbNaslov.TabIndex = 0;
+            this.lbNaslov.Text = "Filmovi zanra:";
+            // 
+            // dgvFilmovi
+            // 
+            this.dgvFilmovi.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFilmovi.Location = new System.Drawing.Point(53, 78);
+            this.dgvFilmovi.MultiSelect = false;
+            this.dgvFilmovi.Name = "dgvFilmovi";
+            this.dgvFilmovi.ReadOnly = true;
+            this.dgvFilmovi.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvFilmovi.Size = new System.Drawing.Size(406, 245);
+            this.dgvFilmovi.TabIndex = 1;
+            // 
+            // btnNovi
+            // 
+            this.btnNovi.Location = new System.Drawing.Point(68, 346);
+            this.btnNovi.Name = "btnNovi";
+            this.btnNovi.Size = new System.Drawing.Size(75, 32);
+            this.btnNovi.TabIndex = 2;
+            this.btnNovi.Text = "Novi";
+            this.btnNovi.UseVisualStyleBackColor = true;
+            this.btnNovi.Click += new System.EventHandler(this.btnNovi_Click);
+            // 
+            // btnIzmeni
+            // 
+            this.btnIzmeni.Location = new System.Drawing.Point(214, 346);
+            this.btnIzmeni.Name = "btnIzmeni";
+            this.btnIzmeni.Size = new System.Drawing.Size(75, 32);
+            this.btnIzmeni.TabIndex = 3;
+            this.btnIzmeni.Text = "Izmeni";
+            this.btnIzmeni.UseVisualStyleBackColor = true;
+            this.btnIzmeni.Click += new System.EventHandler(this.btnIzmeni_Click);
+            // 
+            // btnObrisi
+            // 
+            this.btnObrisi.Location = new System.Drawing.Point(367, 346);
+            this.btnObrisi.Name = "btnObrisi";
+            this.btnObrisi.Size = new System.Drawing.Size(75, 32);
+            this.btnObrisi.TabIndex = 4;
+            this.btnObrisi.Text = "Obrisi";
+            this.btnObrisi.UseVisualStyleBackColor = true;
+            this.btnObrisi.Click += new System.EventHandler(this.btnObrisi_Click);
+            // 
+            // btnRecenzije
+            // 
+            this.btnRecenzije.Location = new System.Drawing.Point(384, 30);
+            this.btnRecenzije.Name = "btnRecenzije";
+            this.btnRecenzije.Size = new System.Drawing.Size(75, 31);
+            this.btnRecenzije.TabIndex = 5;
+            this.btnRecenzije.Text = "Recenzije";
+            this.btnRecenzije.UseVisualStyleBackColor = true;
+            this.btnRecenzije.Click += new System.EventHandler(this.btnRecenzije_Click);
+            // 
+            // FilmForma
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(509, 404);
+            this.Controls.Add(this.btnRecenzije);
+            this.Controls.Add(this.btnObrisi);
+            this.Controls.Add(this.btnIzmeni);
+            this.Controls.Add(this.btnNovi);
+            this.Controls.Add(this.dgvFilmovi);
+            this.Controls.Add(this.lbNaslov);
+            this.Name = "FilmForma";
+            this.Text = "FilmForma";
+            this.Load += new System.EventHandler(this.FilmForma_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFilmovi)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbNaslov;
+        private System.Windows.Forms.DataGridView dgvFilmovi;
+        private System.Windows.Forms.Button btnNovi;
+        private System.Windows.Forms.Button btnIzmeni;
+        private System.Windows.Forms.Button btnObrisi;
+        private System.Windows.Forms.Button btnRecenzije;
+    }
+}
---
+++ FilmsApp/FilmForma.cs
@@ -0,0 +1,106 @@
+﻿using Biznis;
+using DTO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FilmsApp
+{
+    public partial class FilmForma : Form
+    {
+        private FilmBiznis filmBiznis = new FilmBiznis();
+        private ZanrDTO trenutniZanr;
+        public FilmForma(ZanrDTO zanr)
+        {
+            InitializeComponent();
+            trenutniZanr = zanr;
+            lbNaslov.Text = "Filmovi zanra: " + zanr.NazivZanra;
+        }
+
+        private void FilmForma_Load(object sender, EventArgs e)
+        {
+            UcitajPodatke();
+        }
+
+        private void UcitajPodatke()
+        {
+            try
+            {
+                dgvFilmovi.DataSource = filmBiznis.GetAll(trenutniZanr.idZanr);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Greska pri ucitavanju " + ex.Message, "Greska", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnNovi_Click(object sender, EventArgs e)
+        {
+            FilmDetaljiForma forma = new FilmDetaljiForma(trenutniZanr);
+            forma.ShowDialog();
+            UcitajPodatke();
+        }
+
+        private void btnIzmeni_Click(object sender, EventArgs e)
+        {
+            if(dgvFilmovi.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Molimo izaberite film za izmenu!", "Upozorenje", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            FilmDTO selektovani = (FilmDTO)dgvFilmovi.SelectedRows[0].DataBoundItem;
+            FilmDetaljiForma forma = new FilmDetaljiForma(trenutniZanr, selektovani);
+            UcitajPodatke();
+        }
+
+        private void btnObrisi_Click(object sender, EventArgs e)
+        {
+            if (dgvFilmovi.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Molimo izaberite film za izmenu!", "Upozorenje", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            FilmDTO selektovani = (FilmDTO)dgvFilmovi.SelectedRows[0].DataBoundItem;
+
+            DialogResult potvrda = MessageBox.Show("Da li ste sigurni da zelite da obrisete " + selektovani.Naziv + "?", "Potvrda brisanja", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if(potvrda == DialogResult.Yes)
+            {
+                try
+                {
+                    filmBiznis.Delete(selektovani.IdFilm);
+                    MessageBox.Show("Film je uspešno obrisan!",
+                        "Uspeh", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    UcitajPodatke();
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message,
+                        "Greška", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnRecenzije_Click(object sender, EventArgs e)
+        {
+            if (dgvFilmovi.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Molimo izaberite film za pregled recenzija!",
+                    "Upozorenje", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            FilmDTO selektovani = (FilmDTO)dgvFilmovi.SelectedRows[0].DataBoundItem;
+            RecenzijaForma forma = new RecenzijaForma(selektovani);
+            forma.ShowDialog();
+        }
+    }
+}
