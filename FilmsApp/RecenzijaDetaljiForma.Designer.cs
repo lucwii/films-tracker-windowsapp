5414fb38de3b677aa6ca4d86b2cb9fa4bfc1743c
lucwii <milanoviclukaa23@gmail.com>
2026-03-31 12:55:05 +0200

    feat: filmdetalis form added

---
+++ FilmsApp/RecenzijaDetaljiForma.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace FilmsApp
+{
+    partial class RecenzijaDetaljiForma
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbId = new System.Windows.Forms.Label();
+            this.tbId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbTekst = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numOcena = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dtpDatum = new System.Windows.Forms.DateTimePicker();
+            this.btnSacuvaj = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numOcena)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbId
+            // 
+            this.lbId.AutoSize = true;
+            this.lbId.Location = new System.Drawing.Point(40, 44);
+            this.lbId.Name = "lbId";
+            this.lbId.Size = new System.Drawing.Size(21, 13);
+            this.lbId.TabIndex = 0;
+            this.lbId.Text = "ID:";
+            // 
+            // tbId
+            // 
+            this.tbId.Location = new System.Drawing.Point(143, 41);
+            this.tbId.Name = "tbId";
+            this.tbId.ReadOnly = true;
+            this.tbId.Size = new System.Drawing.Size(188, 20);
+            this.tbId.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Tekst recenzije:";
+            // 
+            // tbTekst
+            // 
+            this.tbTekst.Location = new System.Drawing.Point(143, 83);
+            this.tbTekst.Multiline = true;
+            this.tbTekst.Name = "tbTekst";
+            this.tbTekst.Size = new System.Drawing.Size(188, 20);
+            this.tbTekst.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ocena (1-10):";
+            // 
+            // numOcena
+            // 
+            this.numOcena.Location = new System.Drawing.Point(143, 129);
+            this.numOcena.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numOcena.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numOcena.Name = "numOcena";
+            this.numOcena.Size = new System.Drawing.Size(188, 20);
+            this.numOcena.TabIndex = 5;
+            this.numOcena.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 181);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Datum recenzije:";
+            // 
+            // dtpDatum
+            // 
+            this.dtpDatum.Location = new System.Drawing.Point(143, 181);
+            this.dtpDatum.Name = "dtpDatum";
+            this.dtpDatum.Size = new System.Drawing.Size(188, 20);
+            this.dtpDatum.TabIndex = 8;
+            // 
+            // btnSacuvaj
+            // 
+            this.btnSacuvaj.Location = new System.Drawing.Point(143, 237);
+            this.btnSacuvaj.Name = "btnSacuvaj";
+            this.btnSacuvaj.Size = new System.Drawing.Size(75, 30);
+            this.btnSacuvaj.TabIndex = 9;
+            this.btnSacuvaj.Text = "Sacuvaj";
+            this.btnSacuvaj.UseVisualStyleBackColor = true;
+            this.btnSacuvaj.Click += new System.EventHandler(this.btnSacuvaj_Click);
+            // 
+            // RecenzijaDetaljiForma
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 325);
+            this.Controls.Add(this.btnSacuvaj);
+            this.Controls.Add(this.dtpDatum);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numOcena);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbTekst);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbId);
+            this.Controls.Add(this.lbId);
+            this.Name = "RecenzijaDetaljiForma";
+            this.Text = "RecenzijaDetaljiForma";
+            ((System.ComponentModel.ISupportInitialize)(this.numOcena)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbId;
+        private System.Windows.Forms.TextBox tbId;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbTekst;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numOcena;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dtpDatum;
+        private System.Windows.Forms.Button btnSacuvaj;
+    }
+}
---
+++ FilmsApp/RecenzijaDetaljiForma.cs
@@ -0,0 +1,69 @@
+﻿using Biznis;
+using DTO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FilmsApp
+{
+    public partial class RecenzijaDetaljiForma : Form
+    {
+        private RecenzijaBiznis recenzijaBiznis = new RecenzijaBiznis();
+        private FilmDTO trenutniFilm;
+        private RecenzijaDTO trenutnaRecenzija = null;
+        public RecenzijaDetaljiForma(FilmDTO film)
+        {
+            InitializeComponent();
+            trenutniFilm = film;
+            tbId.Visible = false;
+            lbId.Visible = false;
+        }
+
+        public RecenzijaDetaljiForma(FilmDTO film, RecenzijaDTO recenzija)
+        {
+            InitializeComponent();
+            trenutniFilm = film;
+            trenutnaRecenzija = recenzija;
+
+            tbId.Text = recenzija.IdRecenzije.ToString();
+            tbTekst.Text = recenzija.TekstRecenzije;
+            numOcena.Value = recenzija.Ocena;
+            dtpDatum.Value = recenzija.DatumRecenzije;
+        }
+
+        private void btnSacuvaj_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                RecenzijaDTO recenzija = new RecenzijaDTO();
+                recenzija.TekstRecenzije = tbTekst.Text;
+                recenzija.Ocena = (int)numOcena.Value;
+                recenzija.DatumRecenzije = dtpDatum.Value.Date;
+                recenzija.IdFilm = trenutniFilm.IdFilm;
+
+                if(trenutnaRecenzija == null)
+                {
+                    recenzijaBiznis.Insert(recenzija);
+                    MessageBox.Show("Recenzija je uspesno dodata!", "Uspeh", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    recenzija.IdRecenzije = trenutnaRecenzija.IdRecenzije;
+                    recenzijaBiznis.Update(recenzija);
+                    MessageBox.Show("Recenzija je uspesno azurirana!", "Uspeh", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                this.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Greska", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
